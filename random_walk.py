10fa1f8f1d136d4983304134e56a1df02c750be9
Guilherme Luiz Lange <gui.luizlange@gmail.com>
2022-07-11 22:06:57 -0300

    random_walk

--- random_walk.py
+++ random_walk.py
@@ -1,4 +1,5 @@
 from main import read_instance, viable_solution, objetive_function
+import random
 import time
 import sys
 
@@ -13,22 +14,40 @@ execution_time = current_time - initial_time
 desk_count = len(desks)
 test_count = len(tests)
 
-viable_solution = viable_solution(desk_count, desk_count - empty, test_count)
-objetive = objetive_function(distance, similarity, viable_solution)
+
 
 def random_walk(current_solution):
+    idx = random.randint(0, desk_count-1)
+
+    tests_numbers = list(range(1, test_count))
+    tests_numbers.remove(current_solution[idx])
+
+    current_solution[idx] = random.choice(tests_numbers)
     return current_solution
 
-while execution_time < timeout:
-    current_solution = random_walk(viable_solution)
-    solution = objetive_function(distance, similarity, current_solution)
-    print('current_solution', current_solution)
-    print('solution', solution)
+def correct_solution(current_solution):
+    for i in random.sample(range(desk_count), empty):
+        current_solution[i] = 0
+    return current_solution
 
-    
+response_solution = viable_solution(desk_count, desk_count, test_count)
+objetive = objetive_function(distance, similarity, response_solution)
+
+count = 0
+while execution_time < timeout:
+    current_solution = random_walk(response_solution)
+    solution = objetive_function(distance, similarity, current_solution)
+
     if solution < objetive:
+        response_solution = current_solution
         objetive = solution
-        viable_solution = current_solution
-    break
+
+    count += 1
+
+    if count > 4:
+        break
+        
     current_time = time.time()
     execution_time = current_time - initial_time
+
+print('Resposta: ', response_solution, '\n', objetive)
